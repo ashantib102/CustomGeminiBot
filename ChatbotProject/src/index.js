385b11262e6a86688d7bf0901c1451c886deddbb
Ashanti B <44121027+ashantib102@users.noreply.github.com>
2025-04-28 20:34:32 -0400

    Update index.js

--- ChatbotProject/src/index.js
+++ ChatbotProject/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // This is optional; create an index.css file if you have custom styles
-import App from './App'; // Create App.js in src if it doesn’t already exist
+import './index.css'; 
+import App from './App'; 
 
 ReactDOM.render(
   <React.StrictMode>
